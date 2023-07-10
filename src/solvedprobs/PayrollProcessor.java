3b61c1c6afaf14820f3ce5b57298865fafba597e
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-10 16:10:40 +0530

    day2 solved probs on oops

--- src/solvedprobs/PayrollProcessor.java
+++ src/solvedprobs/PayrollProcessor.java
@@ -1,3 +1,14 @@
+/*
+You are asked to create a payroll system for a company. The company has two types of employees: SalariedEmployee and HourlyEmployee. The calculatePay() method is common for both the types
+The SalariedEmployee class has the following attributes:
+name (string)
+salary (double)
+The HourlyEmployee class has the following attributes:
+name (string)
+hourlyRate (double)
+hoursWorked (double)
+The calculatePay() method for SalariedEmployee should calculate the monthly salary, while the calculatePay() method for HourlyEmployee should calculate the weekly pay based on the hourly rate and hours worked. Write a java code snippet to demonstrate the usage of these classes.
+*/
 package solvedprobs;
 
 class Employee{
