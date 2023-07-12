d6f39fe32dacb3c346e6445ccc3d9dc814b60765
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-12 09:57:37 +0530

    day 4 problems solved

--- src/day4/AtmSimulator.java
+++ src/day4/AtmSimulator.java
@@ -1,3 +1,12 @@
+/*
+Write a Java program that simulates an ATM machine. The program should handle the following exceptions:
+InsufficientFundsException: Thrown when a user tries to withdraw an amount greater than their account balance.
+InvalidAmountException: Thrown when a user tries to withdraw a negative or zero amount.
+The program should have the following functionalities:
+Initialise the account balance with a default value.
+Provide options for the user to deposit or withdraw funds.
+Handle exceptions appropriately and display meaningful error messages to the user.
+ */
 package day4;
 
 import java.util.Scanner;
--- src/day4/MessengerAp.java
+++ src/day4/MessengerAp.java
@@ -1,3 +1,10 @@
+/*
+You are expected to create a messaging system that supports multiple messaging services such as SMS, Email, and WhatsApp. The common behaviour is sending messages, but the implementation of sending messages varies. Implement a Java program using interfaces to model this scenario.
+Create an interface called MessagingService with the following method:
+sendMessage(): This method should be implemented by classes representing different messaging services.
+Create three classes, SMSMessagingService, EmailMessagingService, and WhatsAppMessagingService, which implement the MessagingService interface. Implement the sendMessage() method in each class based on the specific messaging service requirements.
+Write a java code to demonstrate the usage of these classes, where you create instances of each messaging service class and invoke the sendMessage() method on each instance.
+ */
 package day4;
 
 interface MessagingService{
