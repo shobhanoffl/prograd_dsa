27211eb2843ea613174bcf8b2b9d3f852e912381
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-14 13:12:58 +0530

    day 6 ongoin

--- src/day6/EquilibriumIndex.java
+++ src/day6/EquilibriumIndex.java
@@ -1,2 +1,24 @@
-package day6;public class EquilibriumIndex {
+package day6;
+
+public class EquilibriumIndex {
+    public static int calculateEquilibriumIndex(int[] array){
+        int[] prefixSumArray = PrefixSum.calculatePrefixSum(array);
+        int c=-1;
+
+//        for (int i=0; i<array.length; i++){
+//            System.out.println(prefixSumArray[i]);
+//        }
+
+        for(int i=1; i<array.length-1; i++){
+            if(prefixSumArray[i-1]==prefixSumArray[array.length-1]-prefixSumArray[i]){
+                c=i;
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{-7, 1, 5, 2, -4, 3, 0};
+        System.out.println(calculateEquilibriumIndex(array));
+    }
 }
--- src/day6/PrefixSum.java
+++ src/day6/PrefixSum.java
@@ -1,2 +1,21 @@
-package day6;public class PrefixSum {
+package day6;
+
+public class PrefixSum {
+
+    static int[] calculatePrefixSum(int[] array){
+        int[] prefixSum = new int[array.length];
+        prefixSum[0]=array[0];
+        for(int i=1;i<array.length;i++){
+            prefixSum[i]=prefixSum[i-1]+array[i];
+        }
+        return prefixSum;
+    }
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5};
+        int[] prefixSum = calculatePrefixSum(array);
+
+        for(int i=0; i<array.length; i++){
+            System.out.println(prefixSum[i]);
+        }
+    }
 }
